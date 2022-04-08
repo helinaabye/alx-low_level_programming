d45e8ab15593813257db52e84253780c9746b952
Helina Abye <helinaago@gmail.com>
2022-04-08 11:54:34 -0700

    add file that prints nums and alpha

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int nu = 0;
+	char ch = 'a';
+
+	while (nu <= 9)
+	{
+		putchar(nu + '0');
+		nu++;
+	}
+
+	while (ch <= 'f')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	putchar('\n');
+	return (0);
+}
