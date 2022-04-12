ae1b3f4e9076c2f7f20887d679625ed8b48e1484
Helina Abye <helinaago@gmail.com>
2022-04-12 13:49:44 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *  * _isalpha - checks for alphabetic character
+ *   * @c: takes in a character
+ *    * Return: 1 for a letter and 0 for other wise
+ *     */
+int _isalpha(int c)
+{
+	int lowerCaseSwitch = 0;
+	int upperCaseSwitch = 0;
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (c == i)
+		{
+			lowerCaseSwitch = 1;
+			break;
+		}
+	}
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		if (c == i)
+		{
+			upperCaseSwitch = 1;
+			break;
+		}
+	}
+	if (lowerCaseSwitch == 0 && upperCaseSwitch == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
