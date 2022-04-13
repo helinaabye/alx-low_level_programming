c19f9b1b290315a61fb87b534773d971d979de3f
Helina Abye <helinaago@gmail.com>
2022-04-13 09:26:48 -0700

    task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * returns the sum two numbers
- * @n is an int
- * @e is an int
+ * add - returns the sum two numbers
+ * @n: is an int
+ * @e: is an int
 */
 
 int add(int n, int e)
