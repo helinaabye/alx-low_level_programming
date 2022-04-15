6d1e7aeb17dbb3248c12c88f18c29e49324938e3
Helina Abye <helinaago@gmail.com>
2022-04-15 03:31:17 -0700

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,9 +11,9 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-		for (i = 1; i <= n; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = 1; j < n; j++)
+			for (j = 1; j < size; j++)
 			{
 				_putchar('#');
 			}
