c430177941b196ad6131a6439730a2ec92ef436c
Helina Abye <helinaago@gmail.com>
2022-04-15 07:23:44 -0700

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,7 +13,7 @@ void print_triangle(int size)
 	{
 		for (i = 1; i <= size; i++)
 		{
-			for (j = size; j >= 1; j--)
+			for (j = size - 1; j >= 1; j--)
 			{
 				if (j > i - 1)
 					_putchar(' ');
