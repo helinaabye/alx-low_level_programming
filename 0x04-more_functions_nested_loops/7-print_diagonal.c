92416630daa4f533b85b1e0ff483d7f5e32f1563
Helina Abye <helinaago@gmail.com>
2022-04-15 03:08:38 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,15 +11,12 @@ void print_diagonal(int n)
 
 	for (i = 1; i <= n; i++)
 	{
+		for (j = 1; j < i; j++)
+		{
+			_putchar(' ');
+		}
+
 		_putchar('\\');
 		_putchar('\n');
-
-		for (j = 0; j < n; j++)
-		{
-			if (j < i)
-			{
-				_putchar(' ');
-			}
-		}
 	}
 }
