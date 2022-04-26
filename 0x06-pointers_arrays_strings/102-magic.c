0cbfb135e8b36eb7410f598356ce4e9066ca0852
Helina Abye <helinaago@gmail.com>
2022-04-26 12:20:52 -0700

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
-/**
- * main - is a function that modifies an array var
- * Return: void
- */
+
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+  int n;
+  int a[5];
+  int *p;
 
-	a[0] = 10;
-	a[1] = 11;
-	a[2] = 12;
-	n = 9;
-	p = &n;
-/*
- * write your line of code here...
- * Remember:
- * - you are not allowed to use a
- * - you are not allowed to modify p
- * - only one statement
- * - you are not allowed to code anything else than this line of code
- */
-	*(p + 5) = 98;
- /* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
