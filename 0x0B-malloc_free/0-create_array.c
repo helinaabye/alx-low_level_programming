24658d2519e598a27bce547c873f3a51e6b00c3c
Helina Abye <helinaago@gmail.com>
2022-05-04 01:48:51 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,11 +10,13 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
-	if (size == 0) 
+	if (size == 0)
 		return (0);
 
 	arr = (char *) malloc(sizeof(char) * size);
 
+	if (arr == 0)
+		return (0);
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
