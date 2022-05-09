583d7c2f9962f101b49ff77f2dceb7d34e6b8f17
Helina Abye <helinaago@gmail.com>
2022-05-09 12:31:26 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - is a structure for a dog object
+ * @name: is the name element
+ * @age: is the age element
+ * @owner: is the owner element
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*#ifndef DOG*/
