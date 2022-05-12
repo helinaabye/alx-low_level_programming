0c91cd74b64fedec14f3a0a672f4276534338da2
Helina Abye <helinaago@gmail.com>
2022-05-12 11:22:38 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
  * print_all - is a function that prints anything
- * @format: list of all arguments passed to the function 
+ * @format: list of all arguments passed to the function
  * Return: void
  */
 void print_all(const char * const format, ...)
