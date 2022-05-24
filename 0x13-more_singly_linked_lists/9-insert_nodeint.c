6bcef23e04ea69a582853412ffde8971170ae4cf
Helina Abye <helinaago@gmail.com>
2022-05-24 04:36:15 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,12 +17,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 
-	if (*head)
+	if (head)
 	{
 		if (idx == 0)
 		{
 			new->next = *head;
-			head = &new;
+			*head = new;
 			return (new);
 		}
 		else
