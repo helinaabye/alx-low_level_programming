015274e4d595a67d979b2ca33a27357a15dc0912
Helina Abye <helinaago@gmail.com>
2022-05-24 04:38:32 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,9 +33,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 				temp = temp->next;
 				i++;
 			}
-			new->next = temp->next;
-			temp->next = new;
-			return (new);
+			while (temp)
+			{
+				new->next = temp->next;
+				temp->next = new;
+				return (new);
+			}
 		}
 	}
 
