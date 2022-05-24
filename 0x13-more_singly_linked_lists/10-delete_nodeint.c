2540a343e0e771e8ec0392af4573f51c4bfa061b
Helina Abye <helinaago@gmail.com>
2022-05-24 06:53:10 -0700

    task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (*head == NULL)
 		return (-1);
-	
+
 	temp = *head;
 	if (index == 0)
 	{
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (temp && i < index - 1)
+	while (temp != NULL && i < index - 1)
 	{
 		temp = temp->next;
 		i++;
