eae156d396a714d3930441425df384606ead8968
Helina Abye <helinaago@gmail.com>
2022-05-24 00:49:54 -0700

    task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 /**
  * add_nodeint -adds a new node at the start of a listint_t list
- * @str: is the number input
+ * @n: is the number input
  * @head: is the pointer to the start of the list
  * Return: the address of the new element, or NULL if it failed
  */
