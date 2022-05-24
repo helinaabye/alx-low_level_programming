99848248d522a722d8f9fc636a7dd56f1e7b9824
Helina Abye <helinaago@gmail.com>
2022-05-24 01:31:10 -0700

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while ((*head) != NULL)
+	while (*head)
 	{
-		temp = *head;
-		*head = (*head)->next;
-		free(temp);
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
 
-	free(*head);
 	*head = NULL;
 }
