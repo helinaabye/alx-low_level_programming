aa18c333897068ee64d3c4fba77223ed2b507bfe
Helina Abye <helinaago@gmail.com>
2022-05-26 11:57:11 -0700

    task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,10 +8,10 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 8 * sizeof(n))
+	if (index > sizeof(n) * 8)
 		return (-1);
 
-	*n |= (index << 1);
+	*n |= (1 << index);
 
 	return (1);
 }
