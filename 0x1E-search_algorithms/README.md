072e25ccfd35a78cfae47189d142be514baf45f7
Helina Abye <helinaago@gmail.com>
2022-11-01 01:40:57 -0700

    task 0 and header file

---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,1 @@
+Practicing search algorithms
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef SEARCH_ALGO
+#define SEARCH_ALGO
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, int min, int max);
+
+#endif /* SEARCH_ALGO */
